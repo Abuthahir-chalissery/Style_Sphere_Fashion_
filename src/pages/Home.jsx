88fc7b626d8aff4e6b3c78ceff60db8581cc75f8
unknown <abuthahirchalissery162@gmail.com>
2025-12-03 14:22:28 +0530

    img bug fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,14 @@ export default function Home() {
             {/* 1 */}
             <div className='hidden sm:block w-full h-auto bg-[#CACDD7] rounded-xl'>
                 <div className='w-full h-full flex justify-center items-end'>
-                    <img className=' w-100% h-auto' src="./src/assets/image1.png" alt="pc-1" />
+                    <img className=' w-100% h-auto' src="./image1.png" alt="pc-1" />
                 </div>
             </div>
 
             {/* 2 */}
             <div className=' w-full h-110 sm:h-auto grid grid-rows-3 '>
                 <div className='w-full h-auto flex justify-center items-start'>
-                    <img className='w-100% h-auto rounded-xl' src="./src/assets/images545.png" alt="" />
+                    <img className='w-100% h-auto rounded-xl' src="./images545.png" alt="" />
                 </div>
                 <div className='w-full h-auto flex flex-col gap-2  justify-center items-center '>
                     <div className='flex flex-col items-center'>
@@ -28,24 +28,24 @@ export default function Home() {
                     </div>
                 </div>
                 <div className='w-full h-auto flex items-end justify-center'>
-                    <img className='w-100% h-auto rounded-xl' src="./src/assets/images1.jpg" alt="" />
+                    <img className='w-100% h-auto rounded-xl' src="./images1.jpg" alt="" />
                 </div>
             </div>
 
             {/* 3 */}
             <div className='hidden sm:block w-full h-auto bg-[#CACDD7] rounded-xl'>
                 <div className='w-full h-full flex justify-center items-end'>
-                    <img className=' w-100% h-auto' src="./src/assets/image3.png" alt="" />
+                    <img className=' w-100% h-auto' src="./image3.png" alt="" />
                 </div>
             </div>
         </div>
 
         <div className='w-full h-auto grid grid-cols-4 sm:flex justify-center gap-5 sm:gap-10 place-items-center place-content-center'>
-            <img className='w-20 h-4'  src="./src/assets/chanel.png" alt="" />
-            <img className='w-20 h-3' src="./src/assets/louis.png" alt="" />
-            <img className='w-20 h-4' src="./src/assets/prada.png" alt="" />
-            <img className='w-20 h-3' src="./src/assets/calvin.png" alt="" />
-            <img className='hidden sm:block w-20 h-4 ' src="./src/assets/denim.png" alt="" />
+            <img className='w-20 h-4'  src="./chanel.png" alt="" />
+            <img className='w-20 h-3' src="./louis.png" alt="" />
+            <img className='w-20 h-4' src="./prada.png" alt="" />
+            <img className='w-20 h-3' src="./calvin.png" alt="" />
+            <img className='hidden sm:block w-20 h-4 ' src="./denim.png" alt="" />
         </div>  
     </div>
     </>
